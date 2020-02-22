cb29b2c337b8f864ec54b5977dd862adadf48560
Luis Couto <couto@utopianmisfits.com>
2020-02-22 12:15:04 +0000

    feat(index): add actionable button to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import styled from "@emotion/styled";
+import Button from "../components/Button";
 
 const Red = styled.div({ color: "red" });
 
-const Index: React.FC<void> = () => <Red>Hello World</Red>;
+const Index: React.FC<void> = () => (
+  <Button>
+    <Red>Hello World</Red>
+  </Button>
+);
 
 export default Index;
